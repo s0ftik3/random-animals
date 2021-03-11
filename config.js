0dd0e8d6b96cbf498975b3be02366d2c35e51b2f
s0ftik3 <slava021323@gmail.com>
2021-03-11 16:25:18 +0600

    1.6.1
    - Minor bug fixes.

--- config.js
+++ config.js
@@ -1,3 +1,11 @@
+const path = require('path');
+const TelegrafI18n = require('telegraf-i18n');
+const i18n = new TelegrafI18n({
+    directory: path.resolve(__dirname, './src/locales'),
+    defaultLanguage: 'en',
+    defaultLanguageOnMissing: true
+});
+
 module.exports = {
     "token": process.env.TOKEN,
     "database": process.env.DATABASE,
